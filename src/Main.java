78f8018965353908b83e1f0c8af581bf7dc5ea16
Семён <karachinstar@yandex.ru>
2023-10-07 11:57:04 +0800

    Внес цикл, чтобы записывать несколько человек

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,20 @@ public class Main {
 
         UserInputReader inputReader = new UserInputConsoleReader(scanner);
         UserDataFileWriter fileWriter = new UserDataFileWriterImpl();
-        String exit = "";
-        while (exit.equals("") || exit.equals("y")) {
+        boolean flag = false;
+        String exit;
+        while (!flag) {
+            //System.out.println(exit);
             try {
                 UserData userData = inputReader.readUserData();
                 fileWriter.writeUserData(userData);
-                System.out.println("Нажмите Y сли хотите добавить еще одного человека " +
+                System.out.println("Введите любой символ хотите добавить еще одного человека " +
                         "или N для выхода");
                 exit = scanner.nextLine();
+                if(exit.equals("n")|exit.equals("N")){
+                    flag = true;
+                }
+                //System.out.println(exit);
             } catch (UserDataException | IOException e) {
                 System.out.println("Ошибка: " + e.getMessage());
             }
